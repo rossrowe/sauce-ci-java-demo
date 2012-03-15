53d4b1ce9f3d870b5fa72b08f5a9b21e46fe9c84
Ross Rowe <piaross@gmail.com>
2012-03-15 13:12:55 +1100

    Reverted back to amazon

--- src/test/java/TunnelTest.java
+++ src/test/java/TunnelTest.java
@@ -33,8 +33,8 @@ public class TunnelTest {
         selenium.type("id=twotabsearchtextbox", "bendis");
         selenium.click("css=input[type=\"image\"]");
         selenium.waitForPageToLoad("30000");
-        selenium.click("link=Scarlet, Book 1");
-        selenium.waitForPageToLoad("30000");
+        //selenium.click("link=Scarlet, Book 1");
+        //selenium.waitForPageToLoad("30000");
         selenium.click("link=New Releases");
         selenium.waitForPageToLoad("30000");
         //selenium.stop();
