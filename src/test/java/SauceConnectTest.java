92691ba65625263255705b1986d84af26da0ef04
Ross Rowe <piaross@gmail.com>
2012-06-29 12:10:01 +1000

    Added missing import statement

--- src/test/java/SauceConnectTest.java
+++ src/test/java/SauceConnectTest.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URL;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
