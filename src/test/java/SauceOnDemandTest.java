e3c74a34a984cc6323f9048ee5fd0765b0b25221
theCampbell <acampbell@saucelabs.com>
2014-11-02 16:39:17 -0800

    yet another typo, missing paren

--- src/test/java/SauceOnDemandTest.java
+++ src/test/java/SauceOnDemandTest.java
@@ -50,8 +50,8 @@ public class SauceOnDemandTest implements SauceOnDemandSessionIdProvider {
             capabilities.setCapability("version", version);
         }
         capabilities.setCapability("platform", Utils.readPropertyOrEnv("SELENIUM_PLATFORM", "XP"));
-        capabilities.setCapability("browserName", Utils.readPropertyOrEnv("SELENIUM_BROWSER", "firefox");
-        capabilities.setCapability("idle-timeout", 90);
+        capabilities.setCapability("browserName", Utils.readPropertyOrEnv("SELENIUM_BROWSER", "firefox"));
+        capabilities.setCapability("idle-timeout", "90");
         String username = Utils.readPropertyOrEnv("SAUCE_USER_NAME", "");
         String accessKey = Utils.readPropertyOrEnv("SAUCE_API_KEY", "");
         this.webDriver = new RemoteWebDriver(
